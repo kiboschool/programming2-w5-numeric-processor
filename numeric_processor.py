6ded60c4dfd770331b47b17b6290138128a16e8a
moltenform (Ben Fisher) <amoltenform@gmail.com>
2023-01-27 16:09:44 -0800

    Revised

--- numeric_processor.py
+++ numeric_processor.py
@@ -2,30 +2,33 @@
 import json
 import urllib.request 
 import time
-from collections import Counter
+
+def computations_list_from_file(filename):
+    with open(filename, 'r') as f:
+        contents = json.load(f)
+        return contents['computations']
 
 class NumericProcessor:
-    def __init__(self, filename):
-        with open(filename, 'r') as f:
-            contents = json.load(f)
-            self.computations = contents['computations']
+    def __init__(self, computations_list):
+        self.last_result = None
+        self.computations = computations_list
     
     def run_computations(self):
-        self.last_result = 0.0
         for computation in self.computations:
             self.last_result = self.run_computation(computation)
     
-    def evaluate_ans(self, value):
-        if value == 'ANS':
-            return self.last_result
-        else:
-            return value
-    
-    def evaluate_ans_list(self, values):
-        return [self.evaluate_ans(val) for val in values]
+    def replace_ans_with_last_result(self, values):
+        new_values = []
+        for value in values:
+            if value == 'ANS':
+                new_values.append(self.last_result)
+            else:
+                new_values.append(value)
+        
+        return new_values
     
     def run_computation(self, computation):
-        values = self.evaluate_ans_list(computation['values'])
+        values = self.replace_ans_with_last_result(computation['values'])
         if computation['operation'] == 'add':
             return self.op_add(values)
         elif computation['operation'] == 'multiply':
@@ -57,22 +60,32 @@ class NumericProcessor:
         print(values[0])
         return values[0]
     
+    def prepareQuery(self, expression):
+        expression = urllib.parse.quote(expression)
+        url = 'http://api.mathjs.org/v4/?expr=' + expression
+        return url
+    
     def op_api_compute(self, values):
         expression = values[0]
-        expression = expression.replace(' ', '')
-        expression = expression.replace('+', '%2b')
-        url = 'http://api.mathjs.org/v4/?expr=' + expression
+        url = self.prepareQuery(expression)
         response = urllib.request.urlopen(url)
         result = response.read().decode('utf-8')
         return float(result)
     
+    
+    
 class NumericProcessorBenchmarkCounts(NumericProcessor):
     def __init__(self, filename):
         super().__init__(filename)
-        self.benchmark_per_operation = Counter()
+        self.benchmark_per_operation = {}
     
     def run_computation(self, computation):
-        self.benchmark_per_operation[computation['operation']] += 1
+        op = computation['operation']
+        if op in self.benchmark_per_operation:
+            self.benchmark_per_operation[op] += 1
+        else:
+            self.benchmark_per_operation[op] = 1
+        
         return super().run_computation(computation)
     
     def show_benchmarks(self):
@@ -81,19 +94,25 @@ class NumericProcessorBenchmarkCounts(NumericProcessor):
 class NumericProcessorBenchmarkTiming(NumericProcessor):
     def __init__(self, filename):
         super().__init__(filename)
-        self.benchmark_per_operation = Counter()
+        self.benchmark_per_operation = {}
         self.benchmark_duration_per_operation = {}
     
     def run_computation(self, computation):
-        self.benchmark_per_operation[computation['operation']] += 1
         started = time.time()
         ret = super().run_computation(computation)
         ended = time.time()
         duration = ended - started
-        if computation['operation'] in self.benchmark_duration_per_operation:
-            self.benchmark_duration_per_operation[computation['operation']] += duration
+        
+        op = computation['operation']
+        if op in self.benchmark_per_operation:
+            self.benchmark_per_operation[op] += 1
         else:
-            self.benchmark_duration_per_operation[computation['operation']] = duration
+            self.benchmark_per_operation[op] = 1
+            
+        if op in self.benchmark_duration_per_operation:
+            self.benchmark_duration_per_operation[op] += duration
+        else:
+            self.benchmark_duration_per_operation[op] = duration
         
         return ret
     
@@ -104,7 +123,8 @@ class NumericProcessorBenchmarkTiming(NumericProcessor):
             print('avg duration (s)', self.benchmark_duration_per_operation[key] / self.benchmark_per_operation[key])
 
 if __name__ == '__main__':
-    o = NumericProcessorBenchmarkTiming('example2.json')
+    computations = computations_list_from_file('example2.json')
+    o = NumericProcessorBenchmarkTiming(computations)
     o.run_computations()
     o.show_benchmarks()
 
